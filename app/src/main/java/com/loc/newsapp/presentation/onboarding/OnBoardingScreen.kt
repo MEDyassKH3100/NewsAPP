efe214ced7636f765307fbcae4e53502221e93ad
Mohamed Yassine Khlaf <150388066+MEDyassKH3100@users.noreply.github.com>
2025-12-05 02:07:44 +0100

    System UI Controller

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -1,11 +1,7 @@
 package com.loc.newsapp.presentation.onboarding
 
-import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,19 +12,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.loc.newsapp.presentation.Dimens.MediumPadding2
-import com.loc.newsapp.presentation.Dimens.MediumPadding3
 import com.loc.newsapp.presentation.common.NewsButton
 import com.loc.newsapp.presentation.common.NewsTextButton
 import com.loc.newsapp.presentation.onboarding.components.OnBoardingPage
@@ -40,14 +31,6 @@ import kotlinx.coroutines.launch
 fun OnBoardingScreen(
     onEvent: (OnBoardingEvent) -> Unit
 ) {
-    val isSystemInDarkMode = isSystemInDarkTheme()
-    val systemUiColor = rememberSystemUiController()
-    SideEffect {
-        systemUiColor.setSystemBarsColor(
-            color = Color.Black.copy(0.1f),
-            darkIcons = isSystemInDarkMode
-        )
-    }
     Column(modifier = Modifier.fillMaxSize()) {
         val pagerState = rememberPagerState(initialPage = 0) {
             pages.size
